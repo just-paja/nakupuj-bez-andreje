8053ecef818378969517aa11f8006831cea85f58
just-paja <pavel@zak.global>
2020-03-24 10:49:44 +0100

    Create zip packages, resize icons

--- scripts/build-package.js
+++ scripts/build-package.js
@@ -4,69 +4,104 @@ const ChromeExtension = require("crx");
 const package = require("../package.json");
 const path = require("path");
 const webpack = require("../webpack.config.js");
+const archiver = require("archiver");
+const Jimp = require("jimp");
 
-const mainIcon = "main.png";
+const mainIconName = "web-bez-andreje";
+const iconSizes = [16, 48, 128];
+
+const mainIcon = `${mainIconName}.png`;
+const mainIcon16 = `${mainIconName}-16.png`;
+const mainIcon48 = `${mainIconName}-48.png`;
+const mainIcon128 = `${mainIconName}-128.png`;
 
 const rootPath = path.resolve(__dirname, "..");
-const mainIconSrc = path.join(rootPath, "icons", mainIcon);
+const mainIconSrc = path.join(rootPath, "icons", "main.png");
 const bundlePath = webpack.output.path;
 const distPath = path.resolve(bundlePath, "..");
 const packagePath = path.join(distPath, `${package.name}.crx`);
+const zipPath = path.join(distPath, `${package.name}.zip`);
 const manifestPath = path.join(bundlePath, "manifest.json");
 const mainIconPath = path.join(bundlePath, mainIcon);
 
-const manifest = {
-  manifest_version: 2,
-  name: "Nakupuj bez Andreje",
-  description: package.description,
-  version: package.version,
-  browser_action: {
-    default_icon: mainIcon
-  },
-  icons: {
-    "16": mainIcon,
-    "48": mainIcon,
-    "128": mainIcon
-  },
-  permissions: [
-    "*://*.itesco.cz/*",
-    "*://itesco.cz/*",
-    "*://*.kosik.cz/*",
-    "*://kosik.cz/*",
-    "*://*.rohlik.cz/*",
-    "*://rohlik.cz/*"
-  ],
-  content_scripts: [
-    {
-      matches: [
-        "https://www.kosik.cz/*",
-        "https://www.rohlik.cz/*",
-        "https://nakup.itesco.cz/*"
-      ],
-      js: ["main.js"],
-      run_at: "document_end"
-    }
-  ],
-  web_accessible_resources: [mainIcon]
-};
+let icons = {};
 
 function createManifest() {
+  const manifest = {
+    manifest_version: 2,
+    name: "Nakupuj bez Andreje",
+    description: package.description,
+    version: package.version,
+    browser_action: {
+      default_icon: mainIcon,
+    },
+    icons,
+    permissions: [
+      "*://*.itesco.cz/*",
+      "*://itesco.cz/*",
+      "*://*.kosik.cz/*",
+      "*://kosik.cz/*",
+      "*://*.rohlik.cz/*",
+      "*://rohlik.cz/*",
+    ],
+    content_scripts: [
+      {
+        matches: [
+          "https://www.kosik.cz/*",
+          "https://www.rohlik.cz/*",
+          "https://nakup.itesco.cz/*",
+        ],
+        js: ["main.js"],
+        run_at: "document_end",
+      },
+    ],
+    web_accessible_resources: [mainIcon],
+  };
   fs.writeFileSync(manifestPath, JSON.stringify(manifest, null, 2));
   fs.copyFileSync(mainIconSrc, mainIconPath);
 }
 
+async function createIcons() {
+  for (size of iconSizes) {
+    const jimp = await Jimp.read(mainIconSrc);
+    const iconName = `${mainIconName}-${size}.png`;
+    jimp.resize(size, size).quality(99).write(path.join(bundlePath, iconName));
+    icons[size] = iconName;
+  }
+}
+
 async function createPackage() {
   const crx = new ChromeExtension({
-    privateKey: process.env.PACKAGE_KEY
+    privateKey: process.env.PACKAGE_KEY,
   });
   await crx.load(bundlePath);
   const buffer = await crx.pack();
-  process.stderr.write(`Creating package in ${packagePath}\n`);
   fs.writeFileSync(packagePath, buffer);
+  process.stderr.write(`Created package in ${packagePath}\n`);
 }
 
-createManifest();
-createPackage().catch(e => {
+async function createZip() {
+  const archive = archiver("zip");
+  const output = fs.createWriteStream(zipPath);
+
+  await new Promise((resolve, reject) => {
+    output.on("close", resolve);
+    output.on("error", reject);
+    archive.directory(bundlePath, false);
+    archive.pipe(output);
+    archive.finalize();
+  });
+  process.stderr.write(`Created package in ${zipPath}\n`);
+}
+
+async function compile() {
+  await createIcons();
+  createManifest();
+  await createPackage();
+  await createZip();
+}
+
+compile().catch((e) => {
   console.error(e);
   process.exit(255);
 });
