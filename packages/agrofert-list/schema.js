7b080770dfe221ddfed33379d1eae69dc4b004cd
just-paja <pavel@zak.global>
2020-03-24 02:05:32 +0100

    Add support for product names

--- packages/agrofert-list/schema.js
+++ packages/agrofert-list/schema.js
@@ -1,28 +1,34 @@
-const { getValidLawForms } = require('./scripts/listSubjects');
+const { getValidLawForms } = require("./scripts/listSubjects");
 
 module.exports = {
-  "id": "/CompanyList",
-  "type": "array",
-  "items": {
-    "type": "object",
-    "properties": {
-      "brandName": {
-        "type": "string",
-        "description": "Název značky - pár slov podle kterých se dá vyhledat v obchodním rejstříku"
+  id: "/CompanyList",
+  type: "array",
+  items: {
+    type: "object",
+    additionalProperties: false,
+    properties: {
+      brandName: {
+        type: "string",
+        description:
+          "Název značky - pár slov podle kterých se dá vyhledat v obchodním rejstříku"
       },
-      "name": {
-        "type": "string",
-        "description": "Celý název společnosti"
+      name: {
+        type: "string",
+        description: "Celý název společnosti"
       },
-      "id": {
-        "type": "string",
-        "description": "IČO"
+      id: {
+        type: "string",
+        description: "IČO"
       },
-      "lawForm": {
-        "type": "string",
-        "description": "Právní forma",
-        "enum": getValidLawForms()
+      lawForm: {
+        type: "string",
+        description: "Právní forma",
+        enum: getValidLawForms()
+      },
+      productNames: {
+        type: "array",
+        items: { type: "string" }
       }
     }
   }
-}
+};
