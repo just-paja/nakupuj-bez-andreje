4c2fa05cf6c053b5241d75010c2cad7c8043e25d
just-paja <pavel@zak.global>
2020-04-08 18:06:42 +0200

    Publish to firefox addons

---
+++ packages/browser-extension/scripts/publish-firefox.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+const { archiveName } = require("../paths");
+const { cmd } = require("web-ext").default;
+
+async function publishFirefoxExtension() {
+  try {
+    const res = await cmd.sign({
+      apiKey: process.env.FIREFOX.API_KEY,
+      apiSecret: process.env.FIREFOX_API_SECRET,
+      id: process.env.FIREFOX_API_UUID,
+      sourceDir: path.resolve(__dirname, "..", "dist", "package"),
+      channel: "listed",
+    });
+    console.log(res);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+}
+
+publishFirefoxExtension();
