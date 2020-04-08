46f0b9e2f8e85b9e94752b0db736140d7770c1d5
just-paja <pavel@zak.global>
2020-04-08 18:11:46 +0200

    Fix typo

--- packages/browser-extension/scripts/publish-firefox.js
+++ packages/browser-extension/scripts/publish-firefox.js
@@ -6,7 +6,7 @@ const { cmd } = require("web-ext").default;
 async function publishFirefoxExtension() {
   try {
     const res = await cmd.sign({
-      apiKey: process.env.FIREFOX.API_KEY,
+      apiKey: process.env.FIREFOX_API_KEY,
       apiSecret: process.env.FIREFOX_API_SECRET,
       id: process.env.FIREFOX_API_UUID,
       sourceDir: path.resolve(__dirname, "..", "dist", "package"),
