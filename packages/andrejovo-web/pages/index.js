3484362bebdef79e855314dd6707f6d39d4df3c8
just-paja <pavel@zak.global>
2020-04-09 02:29:03 +0200

    Improve meta information

--- packages/andrejovo-web/pages/index.js
+++ packages/andrejovo-web/pages/index.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Container from "react-bootstrap/Container";
 
 import { QueryForm } from "../components/QueryForm";
@@ -7,6 +8,13 @@ import "./index.scss";
 
 const Home = () => (
   <Container as="main" className="home-page">
+    <Head>
+      <meta name="title" content="Je to Andrejovo?" />
+      <meta
+        name="description"
+        content="Jednoduchý vyhledavač, který vám pomůže zjistit, jestli produkt patří do Agrofertu Andreje Babiše."
+      />
+    </Head>
     <Banner />
     <QueryForm />
   </Container>
