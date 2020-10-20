5ed140a9e3c3ec54abb256a62ee225c9f907a454
Pavel Žák <pavel@zak.global>
2020-10-20 22:55:33 +0200

    Fix lerna:react-native bindings

--- packages/mobile-app/App.js
+++ packages/mobile-app/App.js
@@ -30,35 +30,6 @@ function App () {
 };
 
 const styles = StyleSheet.create({
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
 });
 
 export default App;
