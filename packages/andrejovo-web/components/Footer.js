7198ee5ce6656936dbd6c552924c65a942aa2bd1
just-paja <pavel@zak.global>
2020-10-25 01:37:55 +0200

    Update footer text to be more actual

--- packages/andrejovo-web/components/Footer.js
+++ packages/andrejovo-web/components/Footer.js
@@ -7,8 +7,7 @@ export function Footer() {
   return (
     <footer className="footer">
       <Container>
-        Projekt <a href="https://andrejovo.cz">Andrejovo</a> má za cíl zmapovat
-        celé impérium Andreje Babiše.
+        Projekt <a href="https://andrejov.cz">Andrejov</a> má za cíl označkovat výrobky Agrofertu.
       </Container>
     </footer>
   );
