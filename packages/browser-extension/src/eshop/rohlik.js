ba23f84f59afb5fb4b90272cbcedd5b9224305a8
Pavel Žák <pavel@zak.global>
2021-02-07 14:05:50 +0100

    Highlight rohlik.cz search
    Fixes #14

--- packages/browser-extension/src/eshop/rohlik.js
+++ packages/browser-extension/src/eshop/rohlik.js
@@ -3,6 +3,7 @@ const { observe } = require('../observer')
 function setup (highlight) {
   if (document.location.href.includes('rohlik')) {
     observe('body', () => {
+      highlight.matchText('.whispererProductLink', '.Whisperer_productName')
       highlight.matchText('.productCard__wrapper', 'h4')
       highlight.matchText('#productDetail > div', 'h2')
     })
